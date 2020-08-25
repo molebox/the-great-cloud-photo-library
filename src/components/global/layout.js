63231093da9775af2e4da5b1a55bb9079ef83cd4
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2020-08-25 20:11:19 +0200

    adding layout

--- src/components/global/layout.js
+++ src/components/global/layout.js
@@ -1,1 +1,21 @@
-// add layout
+import React from 'react';
+import {Box } from '@chakra-ui/core';
+import Header from './header';
+import Footer from './footer';
+import Sidebar from './sidebar'
+
+const Layout = ({children}) => {
+
+    return (
+        <>
+        <Box h="100vh" w="100%" bg="">
+            <Header/>
+            <Sidebar/>
+            {children}
+            <Footer/>
+        </Box>
+        </>
+    )
+}
+
+export default Layout;
