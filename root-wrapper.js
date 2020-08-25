f4246abdfc1b3f7a01011f01072f95e19ec60e56
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2020-08-25 20:18:31 +0200

    added wrap root

---
+++ root-wrapper.js
@@ -0,0 +1,30 @@
+const React = require('react');
+const fetch =require('isomorphic-fetch');
+const {
+    ApolloProvider,
+    ApolloClient,
+    HttpLink,
+    InMemoryCache
+} = require('@apollo/client');
+const {ThemeProvider} = require('@chakra-ui/core');
+const { customTheme } = require('./src/components/global/theme');
+
+
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: new HttpLink({
+        uri: 'the-great-cloud-photo-library.netlify.app/.netlify/functions/',
+        // fetchOptions: {
+        //     mode: 'no-cors',
+        //   }
+    }),
+    fetch
+});
+
+export const wrapRootElement = ({element}) => (
+    <ApolloProvider client={client}>
+        <ThemeProvider theme={customTheme}>
+            {element}
+        </ThemeProvider>
+    </ApolloProvider>
+)
